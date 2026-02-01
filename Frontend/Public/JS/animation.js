216f93998f07a2c548d9946287f7027738d72866
yASHSAINii <1yashsaini23@gmail.com>
2026-02-02 02:58:15 +0530

    home page added

---
+++ Frontend/Public/JS/animation.js
@@ -0,0 +1,35 @@
+
+        const candidate = document.getElementById("candidate");
+        const recruiter = document.getElementById("recruiter");
+
+        const candidateLabel = document.querySelector('label[for="candidate"]');
+        const recruiterLabel = document.querySelector('label[for="recruiter"]');
+        const submitbutton = document.querySelector('#signinbutton')
+
+        function colorswitch() {
+            if (candidate.checked) {
+            candidateLabel.classList.add("bg-blue-500", "text-white", "shadow-md");
+            candidateLabel.classList.remove("bg-white", "text-gray-700");
+
+            submitbutton.classList.add("border-blue-500", "bg-blue-500");
+            submitbutton.classList.remove("border-rose-800", "bg-rose-800");
+
+            recruiterLabel.classList.add("bg-white", "text-gray-700");
+            recruiterLabel.classList.remove("bg-blue-500", "text-white", "shadow-md");
+            } else {
+            recruiterLabel.classList.add("bg-rose-800", "text-white", "shadow-md");
+            recruiterLabel.classList.remove("bg-white", "text-gray-700");
+
+            candidateLabel.classList.add("bg-white", "text-gray-700");
+            candidateLabel.classList.remove("bg-blue-500", "text-white", "shadow-md");
+
+            submitbutton.classList.add("border-rose-800", "bg-rose-800");
+            submitbutton.classList.remove("border-blue-500", "bg-blue-500");
+            }
+        }
+
+        candidate.addEventListener("change", colorswitch);
+        recruiter.addEventListener("change", colorswitch);
+        
+        colorswitch();
+    
