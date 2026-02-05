0dfd1acb1e2e072295f28af22011261bcd08834f
yASHSAINii <1yashsaini23@gmail.com>
2026-02-05 19:59:39 +0530

    test dashboard added and finishing of 2 pages

--- Frontend/Public/JS/animation.js
+++ Frontend/Public/JS/animation.js
@@ -8,23 +8,23 @@
 
         function colorswitch() {
             if (candidate.checked) {
-            candidateLabel.classList.add("bg-blue-500", "text-white", "shadow-md");
+            candidateLabel.classList.add("bg-blue-600", "text-white", "shadow-md");
             candidateLabel.classList.remove("bg-white", "text-gray-700");
 
-            submitbutton.classList.add("border-blue-500", "bg-blue-500");
+            submitbutton.classList.add("border-blue-600", "bg-blue-600");
             submitbutton.classList.remove("border-rose-800", "bg-rose-800");
 
             recruiterLabel.classList.add("bg-white", "text-gray-700");
-            recruiterLabel.classList.remove("bg-blue-500", "text-white", "shadow-md");
+            recruiterLabel.classList.remove("bg-blue-600", "text-white", "shadow-md");
             } else {
             recruiterLabel.classList.add("bg-rose-800", "text-white", "shadow-md");
             recruiterLabel.classList.remove("bg-white", "text-gray-700");
 
             candidateLabel.classList.add("bg-white", "text-gray-700");
-            candidateLabel.classList.remove("bg-blue-500", "text-white", "shadow-md");
+            candidateLabel.classList.remove("bg-blue-600", "text-white", "shadow-md");
 
             submitbutton.classList.add("border-rose-800", "bg-rose-800");
-            submitbutton.classList.remove("border-blue-500", "bg-blue-500");
+            submitbutton.classList.remove("border-blue-600", "bg-blue-600");
             }
         }
 
