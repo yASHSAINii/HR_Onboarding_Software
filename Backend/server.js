e109a049087399c62888e928a71f80c9d4ac6ddc
yASHSAINii <1yashsaini23@gmail.com>
2026-02-16 18:42:10 +0530

    fixed error

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,7 @@
-import express from "express";
-const app = express();
+import app from './app.js';
 
-app.get("/", (req, res)=> {
-    res.send("helllloo");
-});
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
